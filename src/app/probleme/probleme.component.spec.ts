6a1db957fb7686128ee2e9e533b2902e564897d5
Jane <Jane-mary_love@hotmail.com>
2019-03-27 18:07:31 -0400

    test prenom invalid avec 2 caractere ajout de msg erreur

--- src/app/probleme/probleme.component.spec.ts
+++ src/app/probleme/probleme.component.spec.ts
@@ -26,8 +26,10 @@ describe('ProblemeComponent', () => {
   // });
 
   it('champ prenom invalide avec 2 caractères',()=>{
+    let errors = {};
     let zone = component.problemeForm.controls['prenom'];
     zone.setValue('a'.repeat(2));
-    expect(zone.valid).toBeFalsy();
-  })
+    errors = zone.errors || {};
+    expect(errors['minlength']).toBeTruthy();
+  });
 });
