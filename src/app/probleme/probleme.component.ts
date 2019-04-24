8d35e0d033f1f23997569a92eca537b0e1b73661
Jane <Jane-mary_love@hotmail.com>
2019-04-24 14:56:22 -0400

    ajout effet a la page

--- src/app/probleme/probleme.component.ts
+++ src/app/probleme/probleme.component.ts
@@ -26,7 +26,10 @@ export class ProblemeComponent implements OnInit {
         courriel: [{value: '', disabled: true}],
         courrielConfirmation: [{value: '', disabled: true}],
         }),
-       telephone: [{value: '', disabled: true}]
+       telephone: [{value: '', disabled: true}],
+       descriptionProbleme: ['', [Validators.required, Validators.minLength(5)]],
+       noUnite: '',
+       dateProbleme: {value: Date(), disabled: true} 
     });
     this.probleme.obtenirProbleme()
     .subscribe(cat => this.typeProbleme = cat,
