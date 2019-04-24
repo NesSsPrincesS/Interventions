00f53dad0f75376f69c911b261aea38672efb1be
Jane <Jane-mary_love@hotmail.com>
2019-04-24 16:20:43 -0400

    test e2e fait et passe au vert

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to Interventions!');
+    expect(page.getTitleText()).toEqual('Déclarer un problème');
   });
 
   afterEach(async () => {
@@ -20,4 +20,30 @@ describe('workspace-project App', () => {
       level: logging.Level.SEVERE,
     } as logging.Entry));
   });
+
+  it('doit activer le bouton Sauvegarder avec champs valides scénario nominal', () => {
+    page.setChampsValidesScenarioNominal();                    
+    expect(page.boutonSubmit().isEnabled()).toBe(true);
+  });   
+  it(' Doit activer le bouton Sauvegarder avec champs valides scénario alternatif Par message TEXTE', () => {
+    page.setChampsValidesScenarioAlternatifParMessageTexte();                    
+    expect(page.boutonSubmit().isEnabled()).toBe(true);
+  }); 
+  it(' Doit activer le bouton Sauvegarder avec champs valides scénario alternatif Par Par courriel'  , () => {
+    page.setChampsValidesScenarioAlternatifParCourriel();                    
+    expect(page.boutonSubmit().isEnabled()).toBe(true);
+  }); 
+  it('zone prenom a une bordure ROUGE si nombre de caractères insuffisant', () => {
+    page.setZonePrenomCaracteresInsuffisant();  
+    expect(page.obtenirClasseZonePrenom()).toContain('is-invalid');
+  });
+  it(' Zone DESCRIPTION DU PROBLÈME a  une bordure VERTE si nombre de  caractères suffisant  ', () => {
+    page.setZoneDescriptionProblemeCaracteresSuffisants();  
+    expect(page.obtenirClasseZonePrenom()).toContain('is-invalid');
+  });
+  it('zone DESCRIPTION DU PROBLÈME a une bordure ROUGE si nombre de caractères insuffisant', () => {
+    page.setZoneDescriptionProblemeCaracteresInSuffisants();  
+    expect(page.obtenirClasseZoneDescriptionProbleme()).toContain('is-invalid');
+  });
+  
 });
